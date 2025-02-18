504f2f539af710bcfc2d53f349868efe209fa036
sial-sanwal <sialsanwal885@gmail.com>
2025-02-18 12:27:53 +0500

    create the ai agent

--- ai_agent.py
+++ ai_agent.py
@@ -5,12 +5,39 @@ from dotenv import load_dotenv
 load_dotenv()
 # Get the API key
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+TAVILY_API_KEY=os.getenv("TAVILY_API_KEY")
 
 # Check if the key is loaded correctly
 if GROQ_API_KEY:
-    print("API Key loaded successfully")
+    print("GROQ_API_KEY  loaded successfully")
 else:
     print("Error: GROQ_API_KEY not found")
+    
+if TAVILY_API_KEY:
+    print("TAVILY_API_KEY API Key loaded successfully")
+else:
+    print("Error: TAVILY_API_KEY not found")
 # Step 2: Setup LLM & Tools 
+from langchain_groq import ChatGroq
+from langchain_community.tools.tavily_search import TavilySearchResults
+
+groq_llm=ChatGroq(model="llama-3.3-70b-versatile")
+serach_tool=TavilySearchResults(max_results=2)
+
+
 # Step 3: Setup AI Agents with Search Tools and functionality 
+from langgraph.prebuilt import create_react_agent
+from langchain_core.messages.ai import AIMessage
+system_prompt="Act as an AI chatbot who is smart and Friendly"
+agent=create_react_agent(
+    model=groq_llm,
+    tools=[serach_tool],
+    state_modifier=system_prompt
+)
  
+query="tell me about the today date  in pakistan"
+state={"messages":query}
+responce=agent.invoke(state)
+messages=responce.get("messages")
+ai_messages=[message.content for message in messages if isinstance (message,AIMessage)]
+print(ai_messages[-1])
