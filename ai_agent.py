9fdd722ed8bada30aae3bd8f4bacbe47e388a391
sial-sanwal <sialsanwal885@gmail.com>
2025-02-18 11:51:20 +0500

    adding groq api key

--- ai_agent.py
+++ ai_agent.py
@@ -1,3 +1,16 @@
 # Step 1: Setup API Keys for Groq and Tavily 
+
+import os
+from dotenv import load_dotenv
+load_dotenv()
+# Get the API key
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
+# Check if the key is loaded correctly
+if GROQ_API_KEY:
+    print("API Key loaded successfully")
+else:
+    print("Error: GROQ_API_KEY not found")
 # Step 2: Setup LLM & Tools 
-# Step 3: Setup AI Agents with Search Tools and functionality  
+# Step 3: Setup AI Agents with Search Tools and functionality 
+ 
