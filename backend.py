14bf109240108fd4dbe579a87ad36ad91a9fcbea
sial-sanwal <sialsanwal885@gmail.com>
2025-02-18 13:13:08 +0500

    adding pydantic model

--- backend.py
+++ backend.py
@@ -1,3 +1,10 @@
 # Setup Pydantic model 
+from pydantic import BaseModel
+from typing import List
+class RequestState(BaseModel):
+    model_name:str
+    mode_provider:str
+    system_prompt:str
+    message:List[str]
 # Setup AI Agent from FrontEnd Request
 # Run app & Explore Swaggers UI
